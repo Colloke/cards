a0c57231c01eb14c930d3457b44eb72139cb5b75
Colloke <github.p5x18@simplelogin.co>
2023-01-22 12:29:41 -0600

    learning more CSS

---
+++ pages/src/components/Cards.tsx
@@ -0,0 +1,15 @@
+import styles from '@/styles/Home.module.css'
+
+const Cards = () => {
+    return (
+      <div className={styles.cards}>
+        <img card-number="1" className={styles.card} src="https://images.unsplash.com/photo-1516575150278-77136aed6920?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="yoyo"/>
+        <img card-number="2" className={styles.card} src="https://images.unsplash.com/photo-1637066725928-d55765ff664a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="yoyo"/>
+        <img card-number="3" className={styles.card} src="https://images.unsplash.com/photo-1646197879190-78a962aab29b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="yoyo"/>
+        <img card-number="4" className={styles.card} src="https://images.unsplash.com/photo-1646198047133-c0ca69d470f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="yoyo"/>
+        <img card-number="5" className={styles.card} src="https://images.unsplash.com/photo-1646198037117-10070247a9a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="yoyo"/>
+      </div>
+    )
+}
+
+export default Cards;
