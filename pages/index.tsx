c58bf3ae1f25a37050fdef9a6363fee8897caf02
Colloke <github.p5x18@simplelogin.co>
2023-01-23 11:08:39 -0600

    changed the arrows and moved them in placce

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Cards from './src/components/cards'
-import Arrows from './src/components/arrows'
 
 export default function Home() {
   return (
       <main>
         <Cards />
-        <Arrows />
       </main>
   )
 }
--- pages/src/components/cards.tsx
+++ pages/src/components/cards.tsx
@@ -3,6 +3,8 @@ import styles from '@/styles/Home.module.css'
 const Cards = () => {
     return (
       <div className={styles.container}>
+        <a className={`${styles.arrows} ${styles.arrowleft}`} role="button"></a>
+        <a className={`${styles.arrows} ${styles.arrowright}`} role="button"></a>
         <img className={styles.card1} src="https://images.unsplash.com/photo-1516575150278-77136aed6920?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="yoyo"/>
         <img className={styles.card2} src="https://images.unsplash.com/photo-1637066725928-d55765ff664a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="yoyo"/>
         <img className={styles.card3} src="https://images.unsplash.com/photo-1646197879190-78a962aab29b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="yoyo"/>
