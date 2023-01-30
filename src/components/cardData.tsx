c71c03bfc0a39a9511f69dd0a8f490e884ab8d6a
Colloke <github.p5x18@simplelogin.co>
2023-01-30 16:18:16 -0600

    gifs

--- src/components/cardData.tsx
+++ src/components/cardData.tsx
@@ -1,20 +1,20 @@
 export const cardData = [
     {
-        image: 'https://images.unsplash.com/photo-1516575150278-77136aed6920?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+        image: 'https://media2.giphy.com/media/qhPIu3BYfABr36xTJs/giphy.gif'
     },
     {
-        image: 'https://images.unsplash.com/photo-1637066725928-d55765ff664a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+        image: 'https://media4.giphy.com/media/Do2fCvPBz7kur7oESs/giphy.gif'
     },
     {
-        image: 'https://images.unsplash.com/photo-1646197879190-78a962aab29b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+        image: 'https://media4.giphy.com/media/m5G4MgIBj0zFiu9fUn/giphy.gif'
     },
     {
-        image: 'https://images.unsplash.com/photo-1646198047133-c0ca69d470f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+        image: 'https://media1.giphy.com/media/W0RPj8pyB2xvTnqUT0/giphy.gif'
     },
     {
-        image: 'https://images.unsplash.com/photo-1646198037117-10070247a9a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+        image: 'https://media4.giphy.com/media/S4AoOdnnQXVSHxtGMP/giphy.gif'
     },
     {
-        image: 'https://images.unsplash.com/photo-1674978679431-275b931e3031?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+        image: 'https://media1.giphy.com/media/DjvD4j87pNWrM3Quuz/giphy.gif'
     },
 ]
