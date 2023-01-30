0c4e11540d2c3f8a282085fa963c40e231506449
Colloke <github.p5x18@simplelogin.co>
2023-01-30 11:35:41 -0600

    starting to use react fragments

--- pages/src/components/cards.tsx
+++ pages/src/components/cards.tsx
@@ -7,13 +7,7 @@ const Cards = () => {
     <>
     {cardData.map((card, index) => {
       return (
-        <>
-        <img className={styles.card1} src={card.image} alt="Card 1"/>
-        <img className={styles.card2} src={card.image} alt="Card 2"/>
-        <img className={styles.card3} src={card.image} alt="Card 3"/>
-        <img className={styles.card4} src={card.image} alt="Card 4"/>
-        <img className={styles.card5} src={card.image} alt="Card 5"/>
-        </>
+      <img src={card.image} alt='Card 1' className={styles.card} key={index} />
       )
     })}
     </>
