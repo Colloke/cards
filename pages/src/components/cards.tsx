a87b5d70e42b57eb2d81d9888c89e91660a5ea1b
Colloke <github.p5x18@simplelogin.co>
2023-01-30 11:18:13 -0600

    changed around the code in cards.tsx to look better

--- pages/src/components/cards.tsx
+++ pages/src/components/cards.tsx
@@ -1,21 +1,22 @@
+import React from 'react';
 import styles from '@/styles/Home.module.css'
+import { cardData } from './cardData';
 
 const Cards = () => {
-  const handleClick = () => {
-    // Set new className to the card
-  };
    return (
-     <div className={styles.container}>
-          <div>
-            <a className={`${styles.arrows} ${styles.arrowleft}`} role="button" onClick={handleClick}></a>
-            <a className={`${styles.arrows} ${styles.arrowright}`} role="button" onClick={handleClick}></a>
-           </div>
-       <img className={styles.card1} src="https://images.unsplash.com/photo-1516575150278-77136aed6920?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="yoyo"/>
-       <img className={styles.card2} src="https://images.unsplash.com/photo-1637066725928-d55765ff664a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="yoyo"/>
-       <img className={styles.card3} src="https://images.unsplash.com/photo-1646197879190-78a962aab29b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="yoyo"/>
-       <img className={styles.card4} src="https://images.unsplash.com/photo-1646198047133-c0ca69d470f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="yoyo"/>
-       <img className={styles.card5} src="https://images.unsplash.com/photo-1646198037117-10070247a9a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="yoyo"/>
-     </div>
+    <>
+    {cardData.map((card, index) => {
+      return (
+        <>
+        <img className={styles.card1} src={card.image} alt="Card 1"/>
+        <img className={styles.card2} src={card.image} alt="Card 2"/>
+        <img className={styles.card3} src={card.image} alt="Card 3"/>
+        <img className={styles.card4} src={card.image} alt="Card 4"/>
+        <img className={styles.card5} src={card.image} alt="Card 5"/>
+        </>
+      )
+    })}
+    </>
    )
 }
 
