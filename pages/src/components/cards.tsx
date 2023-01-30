9f15a0bc0571d40ebd6869944a9b7e85755efa43
Colloke <github.p5x18@simplelogin.co>
2023-01-30 12:31:13 -0600

    fulling working buttons

--- pages/src/components/cards.tsx
+++ pages/src/components/cards.tsx
@@ -4,18 +4,32 @@ import { cardData } from './cardData';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
 const Cards = ({ slides }: { slides:any }) => {
-const [current, setCurrent] = useState(0)
-const length = cardData.length
+  const [current, setCurrent] = useState(0)
+  const length = cardData.length
 
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
 
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
+
+  if (!Array.isArray(cardData) || cardData.length <= 0) {
+    return null;
+  }
 
    return (
-    <section className="cards">
-      <FaArrowAltCircleLeft className="left-arrow" />
-      <FaArrowAltCircleRight className="right-arrow" />
+    <section className={styles.container}>
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
     {cardData.map((card, index) => {
       return (
-      <img src={card.image} alt='Card 1' className={styles.card} key={index} />
+        <div className={index === current ? 'slide active' : 'slide'} key={index}>
+          {index === current && (
+          <img src={card.image} alt='Card 1' className={styles.card} />
+          )}
+        </div>
       )
     })}
     </section>
