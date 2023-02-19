9dcb3a28b562fdd3ca99e1a2ecc13c6742089e4c
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2023-02-18 18:12:33 -0600

    format fixes

--- src/components/cardData.tsx
+++ src/components/cardData.tsx
@@ -1,65 +1,59 @@
 export const cardData = [
-  {
-    id: 0,
-    image:
-      "https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-24-05.png",
-    gif: "https://media2.giphy.com/media/qhPIu3BYfABr36xTJs/giphy.gif",
-    back: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk",
-  },
-  {
-    id: 1,
-    image:
-      "https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-24-50.png",
-    gif: "https://media4.giphy.com/media/Do2fCvPBz7kur7oESs/giphy.gif",
-    back: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk",
-  },
-  {
-    id: 2,
-    image:
-      "https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-25-17.png",
-    gif: "https://media4.giphy.com/media/m5G4MgIBj0zFiu9fUn/giphy.gif",
-    back: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk",
-  },
-  {
-    id: 3,
-    image:
-      "https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-27-43.png",
-    gif: "https://media1.giphy.com/media/W0RPj8pyB2xvTnqUT0/giphy.gif",
-    back: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk",
-  },
-  {
-    id: 4,
-    image:
-      "https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-28-09.png",
-    gif: "https://media4.giphy.com/media/S4AoOdnnQXVSHxtGMP/giphy.gif",
-    back: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk",
-  },
-  {
-    id: 5,
-    image:
-      "https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-28-46.png",
-    gif: "https://media1.giphy.com/media/DjvD4j87pNWrM3Quuz/giphy.gif",
-    back: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk",
-  },
-];
+    {
+        id: 0,
+        image: 'https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-24-05.png',
+        gif: 'https://media2.giphy.com/media/qhPIu3BYfABr36xTJs/giphy.gif',
+        back: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk',
+    },
+    {
+        id: 1,
+        image: 'https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-24-50.png',
+        gif: 'https://media4.giphy.com/media/Do2fCvPBz7kur7oESs/giphy.gif',
+        back: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk',
+    },
+    {
+        id: 2,
+        image: 'https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-25-17.png',
+        gif: 'https://media4.giphy.com/media/m5G4MgIBj0zFiu9fUn/giphy.gif',
+        back: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk',
+    },
+    {
+        id: 3,
+        image: 'https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-27-43.png',
+        gif: 'https://media1.giphy.com/media/W0RPj8pyB2xvTnqUT0/giphy.gif',
+        back: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk',
+    },
+    {
+        id: 4,
+        image: 'https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-28-09.png',
+        gif: 'https://media4.giphy.com/media/S4AoOdnnQXVSHxtGMP/giphy.gif',
+        back: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk',
+    },
+    {
+        id: 5,
+        image: 'https://raw.githubusercontent.com/DanMyers300/cards/simplify-card-map/src/card_images/Screenshot_2023-01-31_10-28-46.png',
+        gif: 'https://media1.giphy.com/media/DjvD4j87pNWrM3Quuz/giphy.gif',
+        back: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bded83c2-9666-4913-afaf-29ae29e5c24e/defaezm-9dfaea32-8df7-458d-8cbf-a85deba4c0d9.png/v1/fill/w_1024,h_1583,q_80,strp/playing_card_back_by_feivelyn_defaezm-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU4MyIsInBhdGgiOiJcL2ZcL2JkZWQ4M2MyLTk2NjYtNDkxMy1hZmFmLTI5YWUyOWU1YzI0ZVwvZGVmYWV6bS05ZGZhZWEzMi04ZGY3LTQ1OGQtOGNiZi1hODVkZWJhNGMwZDkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-03AHmNDd2ihmIExHIgYsDG3wKdNBsfXHs8M04vkqpk',
+    },
+]
 
 export const textData = [
-  {
-    text: "First Card",
-  },
-  {
-    text: "Second Card",
-  },
-  {
-    text: "Third Card",
-  },
-  {
-    text: "Fourth Card",
-  },
-  {
-    text: "Fifth Card",
-  },
-  {
-    text: "Sixth Card",
-  },
-];
+    {
+        text: 'First Card',
+    },
+    {
+        text: 'Second Card',
+    },
+    {
+        text: 'Third Card',
+    },
+    {
+        text: 'Fourth Card',
+    },
+    {
+        text: 'Fifth Card',
+    },
+    {
+        text: 'Sixth Card',
+    },
+]
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,118 +1,120 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import styles from "@/styles/Home.module.scss";
-import { cardData } from "./cardData";
-import { textData } from "./cardData";
-import classnames from "classnames";
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+import React, { useState } from 'react'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.scss'
+import { cardData } from './cardData'
+import { textData } from './cardData'
+import classnames from 'classnames'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
 
 export const calculateVisibleCardArray = (
-  cardData: any[],
-  selectedIndex: number
+    cardData: any[],
+    selectedIndex: number
 ): any[] => {
-  const leftMostCard = selectedIndex - 2;
-  const rightMostCard = selectedIndex + 2;
-  const visibleCardArray = [];
-  for (let i = leftMostCard; i <= rightMostCard; i++) {
-    visibleCardArray.push(cardData.at(i % cardData.length));
-  }
-  return visibleCardArray;
-};
+    const leftMostCard = selectedIndex - 2
+    const rightMostCard = selectedIndex + 2
+    const visibleCardArray = []
+    for (let i = leftMostCard; i <= rightMostCard; i++) {
+        visibleCardArray.push(cardData.at(i % cardData.length))
+    }
+    return visibleCardArray
+}
 
 const Cards = ({ slides }: { slides: any }) => {
-  const [current, setCurrent] = useState(2);
-  const length = cardData.length;
+    const [current, setCurrent] = useState(2)
+    const length = cardData.length
 
-  const nextSlide = () => {
-    // Switch all the cards up one
-    setCurrent(current === length - 1 ? 0 : current + 1);
-    // Handle the card flip direction
-    document.documentElement.style.setProperty("--flipDirection", "180deg");
-  };
+    const nextSlide = () => {
+        // Switch all the cards up one
+        setCurrent(current === length - 1 ? 0 : current + 1)
+        // Handle the card flip direction
+        document.documentElement.style.setProperty('--flipDirection', '180deg')
+    }
 
-  const prevSlide = () => {
-    // Switch all the cards down one
-    setCurrent(current === 0 ? length - 1 : current - 1);
-    // Handle the card flip direction
-    document.documentElement.style.setProperty("--flipDirection", "180deg");
-  };
+    const prevSlide = () => {
+        // Switch all the cards down one
+        setCurrent(current === 0 ? length - 1 : current - 1)
+        // Handle the card flip direction
+        document.documentElement.style.setProperty('--flipDirection', '180deg')
+    }
 
-  if (!Array.isArray(cardData) || cardData.length <= 0) {
-    return null;
-  }
+    if (!Array.isArray(cardData) || cardData.length <= 0) {
+        return null
+    }
 
-  const visibleCards = calculateVisibleCardArray(cardData, current);
+    const visibleCards = calculateVisibleCardArray(cardData, current)
 
-  return (
-    <>
-      <section className={styles.container}>
-        <FaArrowAltCircleLeft
-          id="leftArrow"
-          className={classnames(styles.arrow, styles.leftarrow)}
-          onClick={nextSlide}
-        />
-        <FaArrowAltCircleRight
-          id="rightArrow"
-          className={classnames(styles.arrow, styles.rightarrow)}
-          onClick={prevSlide}
-        />
+    return (
+        <>
+            <section className={styles.container}>
+                <FaArrowAltCircleLeft
+                    id="leftArrow"
+                    className={classnames(styles.arrow, styles.leftarrow)}
+                    onClick={nextSlide}
+                />
+                <FaArrowAltCircleRight
+                    id="rightArrow"
+                    className={classnames(styles.arrow, styles.rightarrow)}
+                    onClick={prevSlide}
+                />
 
-        {/* This Map currently displays all the cards
+                {/* This Map currently displays all the cards
           We need to display Card2 as a separate div with its own class
           maybe we can use a second map for this?
       
       */}
 
-        <div className={styles.cardContainer}>
-          {visibleCards.map((card, index) => {
-            return (
-              <div
-                key={`Card-${card.id}`}
-                className={classnames(
-                  styles.card,
-                  {
-                    [styles.flipCard]: index === 2,
-                  },
-                  styles[`card${index}`]
-                )}
-              >
-                <Image
-                  src={card.back}
-                  alt={`Card ${index}`}
-                  width="352"
-                  height="493"
-                  className={styles.cardBack}
-                />
-                <Image
-                  src={card.image}
-                  alt={`Card ${index}`}
-                  width="352"
-                  height="493"
-                  className={styles.cardFront}
-                />
-              </div>
-            );
-          })}
-        </div>
-
-        {textData.map((text, index) => {
-          return (
-            <div
-              id="text"
-              className={index === current ? "slide active" : "slide"}
-              key={index}
-            >
-              {index === current && (
-                <div id="text" className={styles.text}>
-                  {text.text}
+                <div className={styles.cardContainer}>
+                    {visibleCards.map((card, index) => {
+                        return (
+                            <div
+                                key={`Card-${card.id}`}
+                                className={classnames(
+                                    styles.card,
+                                    {
+                                        [styles.flipCard]: index === 2,
+                                    },
+                                    styles[`card${index}`]
+                                )}
+                            >
+                                <Image
+                                    src={card.back}
+                                    alt={`Card ${index}`}
+                                    width="352"
+                                    height="493"
+                                    className={styles.cardBack}
+                                />
+                                <Image
+                                    src={card.image}
+                                    alt={`Card ${index}`}
+                                    width="352"
+                                    height="493"
+                                    className={styles.cardFront}
+                                />
+                            </div>
+                        )
+                    })}
                 </div>
-              )}
-            </div>
-          );
-        })}
-      </section>
-    </>
-  );
-};
 
-export default Cards;
+                {textData.map((text, index) => {
+                    return (
+                        <div
+                            id="text"
+                            className={
+                                index === current ? 'slide active' : 'slide'
+                            }
+                            key={index}
+                        >
+                            {index === current && (
+                                <div id="text" className={styles.text}>
+                                    {text.text}
+                                </div>
+                            )}
+                        </div>
+                    )
+                })}
+            </section>
+        </>
+    )
+}
+
+export default Cards
